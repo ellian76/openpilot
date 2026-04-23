9309cf57cc63b75cac767db60f6b4b3cd49c01ee
ellian76 <85388799+ellian76@users.noreply.github.com>
2026-04-23 13:30:08 +0900

    Update ldw.py

--- selfdrive/controls/lib/ldw.py
+++ selfdrive/controls/lib/ldw.py
@@ -29,7 +29,7 @@ class LaneDepartureWarning:
 
       lane_lines = modelV2.laneLines
       l_lane_close = left_lane_visible and (lane_lines[1].y[0] > -(1.08 + CAMERA_OFFSET))
-      r_lane_close = right_lane_visible and (lane_lines[2].y[0] < (1.08 - CAMERA_OFFSET))
+      r_lane_close = right_lane_visible and (lane_lines[2].y[0] < (1.05 - CAMERA_OFFSET))
 
       self.left = bool(l_lane_change_prob > LANE_DEPARTURE_THRESHOLD and l_lane_close)
       self.right = bool(r_lane_change_prob > LANE_DEPARTURE_THRESHOLD and r_lane_close)
